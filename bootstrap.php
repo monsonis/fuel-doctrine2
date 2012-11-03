1eed345cf4892a2243cb08b6b3fcf4731f92f98a
monsonis <monsonis@gmail.com>
2012-11-03 16:03:22 +0100

    Add autoloader for Symfony package, needed for doctrine console

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@ $dir = dirname(__FILE__) . DS;
 
 // Let the FuelPHP autoloader handle loading for Doctrine classes
 Autoloader::add_namespace("Doctrine", $dir . 'Doctrine' . DS, true);
+Autoloader::add_namespace("Symfony", $dir . 'Doctrine/Symfony' . DS, true);
 
 // Set up wrapper namespace
 Autoloader::add_namespace('Doctrine_Fuel', $dir . 'classes' . DS);
